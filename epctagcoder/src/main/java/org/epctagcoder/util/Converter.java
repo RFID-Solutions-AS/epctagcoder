0c1a9d97a1cda12fb488d00e3a7a501110c06a98
Christoffer Holmesland <christoffer.holmesland@gmail.com>
2021-06-23 15:11:47 +0200

    Changed java8 syntax to java6

--- epctagcoder/src/main/java/org/epctagcoder/util/Converter.java
+++ epctagcoder/src/main/java/org/epctagcoder/util/Converter.java
@@ -236,7 +236,14 @@ public class Converter {
    
    
    public static boolean isNumeric(String str) {
-	   return str.chars().allMatch( Character::isDigit );
+		char[] arr = str.toCharArray();
+		for (int i = 0; i < arr.length; i++) {
+			if (!Character.isDigit(arr[i])) {
+				return false;
+			}
+		}
+
+		return true;
    }
    
    
